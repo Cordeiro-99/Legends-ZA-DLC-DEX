f8dff4149a0c901a965f8b583c7fc2afba30de0e
Cordeiro-99 <brunocor1999@gmail.com>
2026-01-16 01:53:34 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,11 +9,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require('./routes/auth');
+const pokedexRoutes = require('./routes/pokedex');
+const userRoutes = require('./routes/user');
+
+app.use('/api/auth', authRoutes);
+app.use('/api/pokedex', pokedexRoutes);
+app.use('/api/user', userRoutes);
+
 app.listen(4000, () => {
   console.log('Server running on port 4000');
 });
-
-const authRoutes = require('./routes/auth');
-const pokedexRoutes = require('./routes/pokedex');
-app.use('/api/auth', authRoutes);
-app.use('/api/pokedex', pokedexRoutes);
